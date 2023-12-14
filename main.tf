d65cbad2b86cc87c418813e4745af5b3a21954bd
RakinKhan <rakinalikhan@gmail.com>
2023-12-14 16:23:00 -0500

    added security group

--- main.tf
+++ main.tf
@@ -67,6 +67,33 @@ resource "aws_route_table" "public_rt" {
 
 # Public Route Table Association
 resource "aws_route_table_association" "public_rta" {
-  subnet_id = aws_subnet.public_subnet.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+# AWS Security Group
+resource "aws_security_group" "security_group_pub" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Practice Security Group"
+  }
 }
