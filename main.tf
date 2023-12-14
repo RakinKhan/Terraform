68699549e8b08d5cec5dca587d91039363cc9aa7
RakinKhan <rakinalikhan@gmail.com>
2023-12-14 09:34:39 -0500

    added resource block for Internet Gateway, Public Route Table, and Route Table Association for public subnet.

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 # Block to create VPC
-resource "aws_vpc" "name_vpc" {
+resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "Practice VPC"
@@ -25,7 +25,7 @@ resource "aws_vpc" "name_vpc" {
 # Block to create Public Subnet
 resource "aws_subnet" "public_subnet" {
   availability_zone = "us-east-1a"
-  vpc_id            = aws_vpc.name_vpc.id
+  vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
 
   tags = {
@@ -36,10 +36,37 @@ resource "aws_subnet" "public_subnet" {
 # Block to create Private Subnet
 resource "aws_subnet" "private_subnet" {
   availability_zone = "us-east-1b"
-  vpc_id            = aws_vpc.name_vpc.id
+  vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
 
   tags = {
     Name = "Private Subnet"
   }
+}
+
+# Block got internet Gateway
+resource "aws_internet_gateway" "public_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "Public Internet Gateway"
+  }
+}
+
+# Route Table (Public)
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.public_gateway.id
+  }
+  tags = {
+    Name = "Public RT"
+  }
+}
+
+# Public Route Table Association
+resource "aws_route_table_association" "public_rta" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
 }
